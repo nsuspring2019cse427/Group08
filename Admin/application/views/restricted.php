2fe910df28b9ebed5fc0077da087cd165b8cc31f
Atiq0009 <islam.atiqul@northsouth.edu>
2019-03-19 21:26:22 +0600

    add restricted.php

---
+++ Admin/application/views/restricted.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Restricted Page</title>
+	<meta charset="utf-8">
+	<title>Department List Show</title>
+	
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>You do not have Permission to access this page.</h1>
+	<a href='<?php echo base_url()."main/login";?>'>Back to LogIn</a>
+
+
+</div>
+
+</body>
+</html>
