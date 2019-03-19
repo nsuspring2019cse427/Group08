94c257499104433e33c80f43c7139ae88b0a9890
Atiq0009 <islam.atiqul@northsouth.edu>
2019-03-19 14:08:43 +0600

    Add about.php file

---
+++ about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>About :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>About</h1>
+        <p>Green Acres is a residential care home specialising in Dementia care, and other mental disorders (excluding learning disabilities). We are located in a fine country house set in four acres of landscaped gardens situated on the outskirts of the beautiful city of Gazipur Dhaka.
+
+There is a range of accommodation, from single and double rooms to self-contained flats; with continual  support from the care home, including meals, activities and housekeeping.
+
+We are dedicated to providing the best residential care for the elderly and are proud of our fully trained and experienced staff, many of whom have been with us for years. Why not visit Our Staff page to see who we are.
+
+Together with our long term care we also offer short term/respite care which gives people the opportunity to experience our excellent facilities. This provides the resident with a holiday and gives the main carer a much needed rest.
+
+We welcome visitors to the home at any time to sample the atmosphere first hand; this can be a drop in for a cup of tea, a meal or even an overnight stay. This way a potential resident will get a good feel of the Home’s environment.</p>
+
+<h4>Confidence in Care</h4>
+
+<p>Our team of well trained staff, are friendly and respectful and dedicated to delivering the highest standard of care and professional support 24 hours a day.
+
+We focus on each individual resident and tailor the care provision in accordance with their individual needs and care plan. We believe it is important to strike a balance between providing the appropriate support and assistance to our residents to help them maintain their independence whilst also encouraging a full and active lifestyle where possible.</p>
+
+<h4>Re-assurance</h4>
+
+<p>Our Home is managed by Affinity Care Management, who maintain excellent quality standards, so we can achieve high levels of satisfaction and comfort for our residents.
+
+Please do get in touch or, better still, come and see Old Alresford Cottage for yourself. We would welcome the opportunity to show you around our residential care home. Contact us now!</p>
+        <a href="index.php" class="btn btn-primary btn-lg btn-block">Back To Home Page</a>
+        
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
