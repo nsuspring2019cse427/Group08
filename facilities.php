453b2d2d9b5f49c644e9994f91a22d0d6ef09520
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 15:36:55 +0600

    add facilities.php file

---
+++ facilities.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Facilities :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>Facilities of the Old Home Care</h1>
+        <p> Recreational and reading facilities such as, televisions, video players, newspapers and books are available. Depending on the extent of the physical activity of the residents other facilties for active sports such as: tennis, table tennis, squash can be provided. Depending on the need billiards and card tables and other entertainment facilities are provided.
+In today's context computers with internet connections are more a necessity than an option especially to receive and send e-mail to the near and dear. The ability to browse the world wide web will allow the residents to be informed and mentally active.
+</p>
+        <h4>List of Facilities</h4>
+        <a href="balanced_food.php" class="btn btn-primary">The Healthy Balanced Food</a>
+        <a href="exercise.php" class="btn btn-default">Exercise Facilities</a>
+        <a href="entertainment.php" class="btn btn-primary">The Entertainment Facilities</a>
+        <a href="treatment.php" class="btn btn-default">Treatment Facilities</a>
+        <a href="index.php" class="btn btn-primary">Back to Home Page</a>
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
