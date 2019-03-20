51542f462a1270e4b516fe6922bd4144f52f73f9
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 15:28:49 +0600

    add  entertainment.php file

---
+++ entertainment.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Entertainment :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>The Entertainment Facilities</h1>
+        <p>
+
+<h4>Music for health</h4>
+
+<p>This is a program that has proved to be very popular in many nursing and residential homes.
+
+It is a session where the residents get to listen to music that they can sing along to, relax to, and they have instruments like tambourines for example that they are encouraged to use along to the music.
+
+Music for Health can be very beneficial in that it helps people remember old times, relax, socialise, have fun and also with coordination and gentle exercise in using the instruments. It is important that there is an area this activity can take place where interruptions are kept to a minimum, as the person leading the session needs to keep everyone's attention.
+Source: Enrique Dans (wikimedia commons)</p>
+<h4>Using the Wii</h4>
+
+<p>Although we don't usually associate elderly people with gaming consoles, this has been a success in many homes brave enough to invest in one!
+
+The advantage of the Wii is the interactive nature of the games, where the participants get some exercise whilst playing, it helps hand to eye coordination and the whole activity can be very sociable. It does require a large screen to play the games on so the residents can see what's happening!</p>
+<h4>Chair Aerobics</h4>
+
+<p>This activity must be led by someone who has proper training in health and fitness in the elderly.
+
+It is designed to increase the heart rate, helps keeps joints and limbs supple, uses muscles to keep them fit, aids coordination and is also great fun for everyone taking part.
+
+All residents in nursing and residential homes must be encouraged to take some exercise whatever their mobility is like, and this is a great way to include the less mobile people in an exercise program that everyone can enjoy.</p>
+<h4>Musical Entertainment</h4>
+
+<p>There are many musicians out there that make their living by going round residential and nursing homes entertaining the residents. This activity is always very popular, being sociable and fun, and helping to keep memories alert by remembering the songs!
+
+This activity is a must for any self respecting activities timetable!
+Source: Paul Townsend (wikimedia commons)</p>
+<h4>Reminiscence</h4>
+
+<p>Reminiscence is an activity is particularly beneficial to those who suffer with dementia.
+
+It involves talking about the past, remembering events in peoples lives, jobs they've had, activities they used to enjoy, people they knew or met, the list is endless! This activity is great with a small group of people, and should include items collected from the "old days" such as carbolic soap, that everyone involved will remember and can touch and smell, and helps get the conversation going.
+
+Losing your memory as you get old can be very distressing and frightening. Reminiscence can bring a lot of comfort, even if some of the memories are sad ones, as everyone in the group will talk and comfort each other if they get sad.
+
+The person leading this activity also needs to be able to turn the mood around to remember happy times too. Other things in the "memory box" could include pictures and ration books, anything that will bring back memories of times gone by.</p>
+        </p>
+        <a href="facilities.php" class="btn btn-primary btn-lg btn-block">Back to Facilities</a>
+        
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
