cebeed5346f87e8a8b1853f25eadb1c31cc4dd6b
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 15:09:51 +0600

    add dinner.php file

---
+++ dinner.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Dinner Food :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>Dinner</h1>
+        <p>Since Dinner is the last meal of the day it’s important to make the right choices because we won’t eat again for at least another 10 ten hours or so.  Ensuring that there is a steady supply of glucose for the body to use as fuel for essential processes whilst we sleep is critical. 
+
+A common cause of waking in the night, often seen in insomnia, is when levels of sugar in the blood fall. The body then has to release stored glucose and this action can cause you to wake and sometimes it may be difficult to get back to sleep.</p>
+
+<p> We consider the health condition of our older citizens. And for dinner we prepared absolutely right food.</p>
+
+<p>To counteract this, it's essential to eat a good dinner which combines both protein (meat, eggs, lentils or beans) and carbohydrates (potatoes, pasta, rice, vegetables and fruit) which will ensure a steady release of glucose to the blood stream - helping to prevent disturbed sleep.</p>
+
+        .</p>
+        </ul>
+        <a href="reg_food.php" class="btn btn-default btn-lg btn-block">Back to Regular Food</a>
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
