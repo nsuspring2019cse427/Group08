a93933fb8f4a22b194f3bf6869de6e8d48121e91
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 15:46:23 +0600

    add  photo_galary.php file

---
+++ photo_galary.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Photo Gallary :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="jumbotron text-center">
+        
+        
+          <h1>Photo Gallary</h1>
+          </div>
+      <div class="row">
+        <div class="col-sm-3">
+          <a href="birthday_img.php" class="thumbnail">
+            <img src="Photo_gallary/Birthday/bday001.png" alt="">
+          </a>
+          <h3>Birthday Celebration Photo</h3>
+          <a href="birthday_img.php" class="btn btn-primary">View Gallary</a>
+        </div>
+        <div class="col-sm-3">
+          <a href="dead_member_photo.php" class="thumbnail">
+            <img src="Photo_gallary/Dead People/ddp001.jpg" alt="">
+          </a>
+          <h3>Dead Members Photo</h3>
+          <a href="dead_member_photo.php" class="btn btn-primary">View Gallary</a>
+        </div>
+        <div class="col-sm-3">
+          <a href="religious_festival_photo.php" class="thumbnail">
+            <img src="Photo_gallary/Festival/festival004.jpg" alt="">
+          </a>
+          <h3>Religious Festivals Photo</h3>
+          <a href="religious_festival_photo.php" class="btn btn-primary">View Gallary</a>
+        </div>
+        <div class="col-sm-3">
+          <a href="special_occation_photo.php" class="thumbnail">
+            <img src="Photo_gallary/Occasion/occa003.jpg" alt="">
+          </a>
+          <h3>Special Occasion Photo</h3>
+          <a href="special_occation_photo.php" class="btn btn-primary">View Gallary</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
