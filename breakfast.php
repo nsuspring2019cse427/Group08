7f1aa31fbcec83a93d3de665cbb9b17c5c5c0057
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 14:48:00 +0600

    add breakfast.php file

---
+++ breakfast.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Breakfast Food :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>Breakfast</h1>
+        <p> 
+
+Older adults may have a difficult time consuming adequate nutrition due to decreased absorption efficiency or high cost of healthy foods. Therefore, seniors are at an increased risk for malnutrition, which can decrease their quality of life. According to the American Academy of Family Physicians, up to 60 percent of home-care elderly patients are malnourished or at risk for becoming malnourished. Inexpensive, soft, easy-to-prepare breakfast foods are ideal for seniors.</p>
+<h4>Eggs</h4>
+
+<p>Eggs make an excellent breakfast food for seniors. A review published in a 2008 issue of “Clinical Nutrition” found that elderly adults may require up to 1.5 grams of protein per kilogram of body weight, or about 0.68 grams of protein per pound, each day, which is almost two times higher than the current protein RDA. Another review published in a 2004 issue of the “Journal of the American College of Nutrition” reports that eggs are ideal for older adults because they are high in protein, easy to prepare and inexpensive. This review also reports that later in life, high blood cholesterol decreases as a risk for heart disease. Seniors who have high cholesterol or heart disease should choose egg whites instead of whole eggs, especially if they consume more than four eggs per week.</p>
+<h4>Oatmeal</h4>
+
+<p>Oatmeal is an excellent breakfast food for seniors because it is fairly inexpensive, easy to chew, easy to prepare, nutrient dense and high in fiber. The National Digestive Diseases Information Clearinghouse reports that low-fiber diets are a main cause of constipation in older adults. The Institute of Medicine encourages adult men ages 50 and older to consume at least 30 grams of fiber and women ages 50 and older to consume at least 21 grams of fiber each day. One packet of regular instant oatmeal provides about 4 grams of fiber.</p>
+<h4>Soft Fresh Fruits</h4>
+
+<p>Fresh fruits are excellent sources of dietary fiber and vitamins for seniors. Soft fresh fruits that are easy to chew and swallow include blueberries, bananas, plums, fresh peaches and kiwi fruit. Sprinkle sliced soft fruits on top of regular oatmeal or breakfast cereal prepared with low-fat milk or soy milk.</p>
+<h4>Smoothies</h4>
+
+<p>High-protein smoothies make a quick breakfast for seniors, don’t require chewing and are easy to prepare using a blender. Ingredients commonly used in breakfast protein smoothies include low-fat milk or soy milk, low-fat yogurt or soy yogurt, soft fruits and peanut butter, almond butter or cashew butter.</p>
+
+
+        </p>
+        </ul>
+        <a href="reg_food.php" class="btn btn-default btn-lg btn-block">Back to Regular Food</a>
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
