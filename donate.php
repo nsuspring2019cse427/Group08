757c7cd903b63c291ada1daebd97d377bd37d074
TahsinaSabrin <tahsina.sabrin@northsouth.edu>
2019-03-20 15:13:17 +0600

    add  donate.php file

---
+++ donate.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Donate :: OAHM</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          
+          <a href="index.php" class="navbar-brand">Green Acres Old Care Home</a>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-12">
+        <h1>Donate</h1>
+        <p>From fancy cars and expensive clothes to fine dining and exotic vacations, there are many ways you can spend your hard-earned money. And there is nothing wrong with rewarding yourself for a job well done -- but what would happen if you rewarded someone else instead?
+
+A recent study by Harvard Business School faculty and graduate students titled “Feeling Good about Giving: The Benefits (and Costs) of Self-Interested Charitable Behavior," explores the ways in which charitable behavior can lead to benefits for the giver. While the the concept that giving to others can make you feel good about yourself is not revolutionary, there are several more subtle ways that giving your money or time for a cause can benefit your psychological, spiritual and emotional well-being.</p>
+
+<p>Please Donate Money For Our Old Care Home.</p> 
+        <a href="donate_org.php" class="btn btn-primary btn-lg btn-block">Donate for Organization</a>
+        <a href="donate_member.php" class="btn btn-default btn-lg btn-block">Donate for Particular Member</a>
+        <a href="donor_view.php" class="btn btn-primary btn-lg btn-block">View Donor List</a>
+        <a href="index.php" class="btn btn-default btn-lg btn-block">Back to Home Page</a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+      <div class="container">
+        <div class="navbar-text pull-left">
+          <p>&copy; 2015 Green Acres Old Care Home</p>
+        </div>
+        <div class="navbar-text pull-right">
+          <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+          <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+        </div>
+        
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
